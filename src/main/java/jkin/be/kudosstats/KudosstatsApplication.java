759b50776e6ddfc96a54c6c73e042565604a0024
pablovillazon <p.villazon@gmail.com>
2019-09-22 08:44:01 -0400

    Added maven files for init project

---
+++ src/main/java/jkin/be/kudosstats/KudosstatsApplication.java
@@ -0,0 +1,13 @@
+package jkin.be.kudosstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KudosstatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KudosstatsApplication.class, args);
+	}
+
+}
